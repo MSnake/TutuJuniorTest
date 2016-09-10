9cd570c304e343da08bfe8a72c073febf89a696b
Alex <MSnakeDT@gmail.com>
2016-09-10 18:32:09 +0300

    Добалвено боковое меню

--- app/src/main/java/com/workspace/alex/tutujuniortest/MainActivity.java
+++ app/src/main/java/com/workspace/alex/tutujuniortest/MainActivity.java
@@ -1,45 +1,65 @@
 package com.workspace.alex.tutujuniortest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.workspace.alex.tutujuniortest.utils.JSONDataLoad;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-import org.json.JSONException;
-
-import java.io.IOException;
-
-public class MainActivity extends AppCompatActivity {
-
-    private static final String IOTAG = "mainActivityIO";
-    private static final String JSONTAG = "mainActivityJSON";
-
-    private final  String  filename = "fromStationSmall.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button initBtn = (Button) findViewById(R.id.init_button);
-        initBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-                //Выполняем инициализацию хранилищ данных
-                try {
-                    JSONDataLoad.initDatas(getApplicationContext());
-                    Toast.makeText(getApplicationContext(),"Загружено",Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    Log.d(IOTAG, "IO Exeption " + e.toString());
-                } catch (JSONException e) {
-                    Log.d(IOTAG, "JSON Exeption " + e.toString());
-                }
 
-            }
-        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_timing) {
+            // Handle the camera action
+        } else
+        if (id == R.id.nav_about)
+        {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/test/java/com/workspace/alex/tutujuniortest/DataLoadTest.java
+++ app/src/test/java/com/workspace/alex/tutujuniortest/DataLoadTest.java
@@ -2,10 +2,14 @@ package com.workspace.alex.tutujuniortest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
 
+import com.workspace.alex.tutujuniortest.data.ArrivelData;
+import com.workspace.alex.tutujuniortest.data.DepartureData;
 import com.workspace.alex.tutujuniortest.utils.JSONDataLoad;
 
 
+import org.json.JSONException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,50 +19,33 @@ import java.io.IOException;
 /**
  * Created by Alex on 08.09.2016.
  */
-public class DataLoadTest extends Assert {
-
-    private String inputFrom="";
-    private String inputTo="";
-    private Context context;
-    private MainActivity activity;
+public class DataLoadTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
 
-    @Before
-    public  void setUpApp()
-    {
-        inputFrom = "fromStations.json";
-        inputTo = "toStations.json";
-        context = activity.getApplicationContext();
+    private MainActivity mActivity;
+
+    public DataLoadTest(){
+        super(MainActivity.class);
     }
 
-    @Before
-    public  void setUpPaths()
-    {
-        inputFrom = "fromStations.json";
-        inputTo = "toStations.json";
-        context = activity.getApplicationContext();
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = getActivity();
+        JSONDataLoad.initDatas(mActivity.getApplicationContext());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
     }
 
     @Test
-    public void testLaunchApp()
-    {
-
-
+    public void initDatas() {
+        assertEquals(ArrivelData.getInstance().getData().size(),439);
+        assertEquals(DepartureData.getInstance().getData().size(),439);
     }
 
-    @Test
-    public void testGettingAssertFromPath() throws IOException {
-        String str = JSONDataLoad.AssetJSONFile(inputFrom, context);
-        System.out.println(str);
-
-    }
-
-    @Test
-    public void testGettingAssertToPath() throws IOException {
-        String str = JSONDataLoad.AssetJSONFile(inputTo, context);
-        System.out.println(str);
-
-    }
 
 
 
