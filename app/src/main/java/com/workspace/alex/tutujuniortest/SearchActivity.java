80aed6858ad2fc153eee5d5fe29070749627f69f
Alex <MSnakeDT@gmail.com>
2016-09-12 23:47:35 +0300

    Переработано нажатие кнопки home в SearchActivity теперь двгаемся по стеку фрагментов, если стек пуст - завершаем активность

--- app/src/main/java/com/workspace/alex/tutujuniortest/SearchActivity.java
+++ app/src/main/java/com/workspace/alex/tutujuniortest/SearchActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.workspace.alex.tutujuniortest.fragments.SearchFragment;
 import com.workspace.alex.tutujuniortest.models.CityModel;
@@ -38,6 +39,26 @@ public class SearchActivity extends AppCompatActivity {
         SearchFragment fragment = SearchFragment.newInstance(data);
         fragmentManager.beginTransaction().add(R.id.fragmentContainerSecond, fragment).commit();
         Log.d(TAG,"Количество транзакций в ФМ "+fragmentManager.getBackStackEntryCount());
+    }
+
+
+    //
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //При нажатии кнопки home(стрелка назад)
+            case android.R.id.home:
+                //Смотри бэк стэк фрагментов, если есть возвращаемся на 1, если нет,
+                //значит мы в корне стека фрагментов --> завершить активность
+                if (getFragmentManager().getBackStackEntryCount() > 0) {
+                    getFragmentManager().popBackStack();
+                } else {
+                    finish();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
